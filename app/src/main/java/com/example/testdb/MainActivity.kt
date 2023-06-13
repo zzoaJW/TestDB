5f46c921bda10009869c21da4dfdd82fbfe6f2dd
zzoaJW <i7can7i@gmail.com>
2023-06-13 09:36:52 +0900

    저장/조회 기능 구현

--- app/src/main/java/com/example/testdb/MainActivity.kt
+++ app/src/main/java/com/example/testdb/MainActivity.kt
@@ -10,9 +10,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dbHelper = DBHelper(this,1)
+
+        binding.btnSave.setOnClickListener {
+            dbHelper.insert(binding.editTxt1.text.toString(), Integer.parseInt(binding.editTxt2.text.toString()), binding.editTxt3.text.toString())
+        }
+
+        binding.btnSelect.setOnClickListener {
+            binding.resultSelect.text = dbHelper.result
+        }
 
 
     }
